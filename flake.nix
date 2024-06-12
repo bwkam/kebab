c5d7e816cfc0203295142d57a193a5a2772f58a7
bwkam <userdev987@gmail.com>
2024-06-12 22:36:57 +0300

    first

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "haxe bad, kebab good";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    haxe-master-src = {
+      type = "git";
+      url = "https://github.com/HaxeFoundation/haxe";
+      submodules = true;
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    inherit (nixpkgs) lib;
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+
+    perSystem = f: lib.genAttrs systems f;
+  in {
+    packages = perSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      haxe = pkgs.callPackage ./haxe;
+    });
+  };
+}
---
+++ haxe/default.nix
@@ -0,0 +1,11 @@
+{
+  haxe,
+  haxe-master-src,
+  ocaml-ng,
+}: {
+  master = haxe.overrideAttrs (final: prev: {
+    version = "master";
+    src = "${haxe-master-src}";
+    buildInputs = (prev.buildInputs or []) ++ (with ocaml-ng.ocamlPackages_4_14; [terminal_size ipaddr]);
+  });
+}
