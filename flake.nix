6382c5dddf3b8e22b3ae15925170560693157c39
bwkam <userdev987@gmail.com>
2024-06-13 12:53:29 +0300

    add haxe nightly

--- flake.nix
+++ flake.nix
@@ -3,12 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
     haxe-master-src = {
       type = "git";
       url = "https://github.com/HaxeFoundation/haxe";
       submodules = true;
       flake = false;
     };
+    haxe-nightly-src = {
+      type = "git";
+      url = "https://github.com/HaxeFoundation/haxe?ref=development";
+      submodules = true;
+      flake = false;
+    };
+
     kha-src = {
       type = "git";
       url = "https://github.com/Kode/Kha.git?ref=main";
@@ -34,6 +42,7 @@
     self,
     nixpkgs,
     haxe-master-src,
+    haxe-nightly-src,
     kha-src,
     kode_haxe-src,
     khamake-src,
@@ -54,10 +63,18 @@
       haxe_master =
         (import ./haxe
           {
-            inherit haxe-master-src;
+            inherit haxe-master-src haxe-nightly-src;
             inherit (pkgs) ocaml-ng haxe;
           })
         .master;
+
+      haxe_nightly =
+        (import ./haxe
+          {
+            inherit haxe-master-src haxe-nightly-src;
+            inherit (pkgs) ocaml-ng haxe;
+          })
+        .nightly;
       kha = pkgs.callPackage ./kha {
         inherit pkgs kha-src kode_haxe-src khamake-src;
       };
--- haxe/default.nix
+++ haxe/default.nix
@@ -1,11 +1,17 @@
 {
   haxe,
   haxe-master-src,
+  haxe-nightly-src,
   ocaml-ng,
 }: {
   master = haxe.overrideAttrs (final: prev: {
     version = "master";
     src = "${haxe-master-src}";
+    buildInputs = (prev.buildInputs or []) ++ (with ocaml-ng.ocamlPackages_4_14; [terminal_size ipaddr]);
+  });
+  nightly = haxe.overrideAttrs (final: prev: {
+    version = "master";
+    src = "${haxe-nightly-src}";
     buildInputs = (prev.buildInputs or []) ++ (with ocaml-ng.ocamlPackages_4_14; [terminal_size ipaddr]);
   });
 }
