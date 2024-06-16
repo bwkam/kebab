1698fb606f14189a7a92b691f1e2b1ab39c399ac
bwkam <userdev987@gmail.com>
2024-06-16 15:29:54 +0300

    remove unused final argument

--- haxe/default.nix
+++ haxe/default.nix
@@ -4,12 +4,12 @@
   haxe-nightly-src,
   ocaml-ng,
 }: {
-  master = haxe.overrideAttrs (final: prev: {
+  master = haxe.overrideAttrs (_: prev: {
     version = "master";
     src = "${haxe-master-src}";
     buildInputs = (prev.buildInputs or []) ++ (with ocaml-ng.ocamlPackages_4_14; [terminal_size ipaddr]);
   });
-  nightly = haxe.overrideAttrs (final: prev: {
+  nightly = haxe.overrideAttrs (_: prev: {
     version = "nightly";
     src = "${haxe-nightly-src}";
     buildInputs = (prev.buildInputs or []) ++ (with ocaml-ng.ocamlPackages_4_14; [terminal_size ipaddr]);
